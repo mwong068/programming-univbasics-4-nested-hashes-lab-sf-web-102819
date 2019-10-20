7f6325a53754015f2720122afbb319696e14ff7d
Megan Wong <mwong068@gmail.com>
2019-10-20 02:36:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,7 +88,7 @@ def adding_matz
         }
      }
 
-programmer_hash.merge!({:yukihiro_matsumoto = {
+programmer_hash.merge!({:yukihiro_matsumoto => {
   :known_for => "Ruby",
   :languages => ["LISP", "C"]
 } })
